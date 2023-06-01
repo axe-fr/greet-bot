02bed917eeeb5f2005acd8c659ea96aefd6d1ebc
Άξονας <128817936+axe-fr@users.noreply.github.com>
2023-06-01 17:58:03 +0200

    Update ready.ts

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -5,11 +5,7 @@ const event: BotEvent = {
     name: Events.ClientReady,
     once: true,
     async execute(client: Client) {
-    const webhook = new WebhookClient({id:"1112068712791552061", token:"vGP2IT-ORnbkBOP5metNIjIT_TBrwwVljaBh9pDOSwediSDUF5qL5VWvOsGcptMcGax5"});
-
-    webhook.send({content:`\`${client.user.username}\` est connecté.`})
-
-
+   
     console.log((`Connecté ${client.user?.tag}`))
     },
 }
